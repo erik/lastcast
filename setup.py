5774312385323967d150a158dd7d1bc0e0b4cfa4
Erik Price <github@erikprice.net>
2017-10-13 18:26:40 -0700

    v0.3.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='lastcast',
-    version='0.3.0',
+    version='0.3.1',
     description='Scrobble music to last.fm from Chromecast.',
     author='Erik Price',
     url='https://github.com/erik/lastcast',
