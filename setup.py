6aab3aaac607b8964f5844a3537ec1570684e352
dewdrop <81049573+dewdropawoo@users.noreply.github.com>
2022-07-17 15:52:28 -0700

    Bump minimum required Python to 3.7
    PyLast 5.0.0 explicitly requires 3.7+, bumping the check here to match

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ import sys
 from setuptools import setup
 
 
-# Be verbose about Python < 3.6 being deprecated.
-if sys.version_info < (3, 6):
+# Be verbose about Python < 3.7 being deprecated.
+if sys.version_info < (3, 7):
     print('\n' * 3 + '*' * 64)
-    print('lastcast requires Python 3.6+, and might be broken if run with\n'
+    print('lastcast requires Python 3.7+, and might be broken if run with\n'
           'this version of Python.')
     print('*' * 64 + '\n' * 3)
 
